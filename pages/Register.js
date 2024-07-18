7db5659e6d0befd3fe3521e116b4104cb5226fdf
Martin Kitonga <119602263+blacksnowmartin@users.noreply.github.com>
2024-07-19 02:08:09 +0300

    Create Register.js

---
+++ pages/Register.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Register() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res
